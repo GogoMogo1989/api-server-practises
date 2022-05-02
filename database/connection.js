cdb284c7722bd0db58af7b72b15275c098ee8ce2
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-05-02 17:25:58 +0200

    20220502

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL || "mongodb://localhost/api-database", {useNewUrlParser: true})
+        await mongoose.connect(process.env.DB_URL || "mongodb://127.0.0.1/api-database", {useNewUrlParser: true})
         console.log("DB Connected");
     } catch (error) {
         console.log("DB Connect error", error)
